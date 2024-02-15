d608e3426277cc91c9d95a383713199f600ce3a0
THMonster <me@isoasflus.com>
2024-02-15 15:38:44 +0800

    update start script

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 def main [] {
     [1, 2] | par-each { |it|
 	    if $it == 1 {
-		    ./GameServer/bin/Debug/net8.0/GameServer.exe
+		    cd GameServer/bin/Debug/net8.0
+		    GameServer.exe
 		} else if $it == 2 {
-		    ./SDKServer/bin/Debug/net8.0/SDKServer.exe
+		    cd SDKServer/bin/Debug/net8.0
+		    SDKServer.exe
 		}
 	}
 }
